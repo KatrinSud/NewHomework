a8d50cc45cf21c76b554dcb07f665a61249f68af
Katya <katringr-a17@yandex.ru>
2023-12-23 15:32:55 +0300

    Решение первой задачи

--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
-﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа
+//  в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+// int firstElement = 4;
+// int secondElements = 15;
+
+// PrintNumbers(firstElement,secondElements);
+
+// void PrintNumbers(int startElement,int endElement)
+// {
+//    System.Console.WriteLine(startElement);
+
+//    if(endElement <= startElement)
+//    {
+//       return;
+//    }
+
+//    PrintNumbers(startElement+1, endElement);
+// }
+
+
 
 // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
